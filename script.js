ee8c8e70b3451e04517de50e820c1f83fe44f048
Rakesh Kumar Mahto <rakeshmahto3636@gmail.com>
2026-03-04 21:08:22 +0530

    added keyboard input

--- script.js
+++ script.js
@@ -8,19 +8,82 @@ buttons.forEach(button => {
     });
 });
 
+document.addEventListener("keydown", function (Event) {
+    calculate(Event.key);
+});
+
 function calculate(key){
     switch (key) {
         case "=":
             display.value=eval(display.value);
             break;
+        case 'Enter':
+            display.value=eval(display.value);
+            break;
         case "AC":
+            display.value="";
+            break;
+        case 'Escape':
             display.value="";
             break;
         case "D":
             display.value=display.value.slice(0,-1);
             break;
-        default:
+        case 'Backspace':
+            display.value=display.value.slice(0,-1);
+            break;
+        case "(":
             display.value+=key;
+            break;
+        case ")":
+            display.value+=key;
+            break;
+        case "/":
+            display.value+=key;
+            break;
+        case "7":
+            display.value+=key;
+            break;
+        case "8":
+            display.value+=key;
+            break;       
+        case "9":
+            display.value+=key;
+            break;
+        case "*":
+            display.value+=key;
+            break;
+        case "4":
+            display.value+=key;
+            break;
+        case "5":
+            display.value+=key;
+            break;
+        case "6":
+            display.value+=key;
+            break;
+        case "-":
+            display.value+=key;
+            break;
+        case "1":
+            display.value+=key;
+            break;
+        case "2":
+            display.value+=key;
+            break;
+        case "3":
+            display.value+=key;
+            break;
+        case "+":
+            display.value+=key;
+            break;
+        case "0":
+            display.value+=key;
+            break;
+        case ".":
+            display.value+=key;
+            break;
+        default:
             break;
     }
     
